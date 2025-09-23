b83f7a001bfe1b31f2311fafa982f39e86cf3a91
Joshshegz <adesanwojoshua21@gmail.com>
2025-09-23 14:09:27 +0100

    pagination fix

--- blog/models.py
+++ blog/models.py
@@ -39,7 +39,8 @@ class Blog(models.Model):
 
 
     def get_absolute_url(self):
-        return reverse( 'blog:post_detail', args=[self.id] )
+        return reverse( 'blog:post_detail', args=[self.publish.year,
+        self.publish.month, self.publish.day, self.slug])
 
 # class Profile(models.Model):
 #     user = models.OneToOneField(User, on_delete=models.CASCADE)
